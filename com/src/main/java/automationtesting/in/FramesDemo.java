ac64d9fa404f5c6b0a189194b137596036971e58
LILT-71 <LILT-71@Leadit>
2018-09-17 12:40:22 +0530

    modified

--- com/src/main/java/automationtesting/in/FramesDemo.java
+++ com/src/main/java/automationtesting/in/FramesDemo.java
@@ -9,52 +9,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FramesDemo {
 	static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
-		  driver=new ChromeDriver();
-	      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		  driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		  driver.manage().deleteAllCookies();
-		  driver.manage().window().maximize();
-		  driver.get("http://demo.automationtesting.in/Frames.html");
-		 /* int total_frames=driver.findElements(By.tagName("iframe")).size();
-		  System.out.println("total frames in a page is "+total_frames);
-		  driver.switchTo().frame("SingleFrame");
-		  driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys("siva");
-         
-          driver.switchTo().defaultContent();
-     
-     
-		  driver.findElement(By.xpath("//a[contains(text(),'Iframe with in an Iframe')]")).click();
-		  driver.switchTo().frame(1);
-		  driver.switchTo().frame(0);
-		
-		  driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys("sivaramakrishna");
-		  Thread.sleep(2000);
-		  */
-		  
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("http://demo.automationtesting.in/Frames.html");
+		/*
+		 * int total_frames=driver.findElements(By.tagName("iframe")).size();
+		 * System.out.println("total frames in a page is "+total_frames);
+		 * driver.switchTo().frame("SingleFrame");
+		 * driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys(
+		 * "siva");
+		 * 
+		 * driver.switchTo().defaultContent();
+		 * 
+		 * 
+		 * driver.findElement(By.
+		 * xpath("//a[contains(text(),'Iframe with in an Iframe')]")).click();
+		 * driver.switchTo().frame(1); driver.switchTo().frame(0);
+		 * 
+		 * driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys(
+		 * "sivaramakrishna"); Thread.sleep(2000);
+		 */
+
 		switchToFrame(0);
 		driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys("siva");
-        switchToDefaultContent();
-        
-        driver.findElement(By.xpath("//a[contains(text(),'Iframe with in an Iframe')]")).click();
-		  
-		  switchToFrame(1);
-		  switchToFrame(0);
-		  
-		  driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys("sivaramakrishna");
-		  Thread.sleep(2000);
-	}
-		  
-		  public static void switchToFrame(int index) {
-			  
-			  driver.switchTo().frame(index);
-		  }
-		  
-		  public static void switchToDefaultContent() {
-			  driver.switchTo().defaultContent();
-		  }
-	
+		switchToDefaultContent();
+
+		driver.findElement(By.xpath("//a[contains(text(),'Iframe with in an Iframe')]")).click();
+
+		switchToFrame(1);
+		switchToFrame(0);
+
+		driver.findElement(By.xpath("html/body/section/div/div/div/input")).sendKeys("sivaramakrishna");
+		Thread.sleep(2000);
 	}
 
+	public static void switchToFrame(int index) {
 
+		driver.switchTo().frame(index);
+	}
+
+	public static void switchToDefaultContent() {
+		driver.switchTo().defaultContent();
+	}
+
+}
--- com/src/main/java/sampleselenium/com/Amazon.java
+++ com/src/main/java/sampleselenium/com/Amazon.java
@@ -14,59 +14,62 @@ import junit.framework.Assert;
 
 public class Amazon {
 	WebDriver driver;
+
 	@BeforeTest
-	public void setUp() {   
+	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-		driver=new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.navigate().to("https://www.amazon.in/");
-	   	
+
 	}
-	@Test(priority=1)
+
+	@Test(priority = 1)
 	public void searchboxAmazonTest() {
 		driver.findElement(By.xpath(".//*[@id='twotabsearchtextbox']")).sendKeys("redmi note5");
 		driver.findElement(By.xpath("//input[@type='submit' and @value='Go']")).click();
 	}
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void verifyLandingPageTest() {
-		boolean b=driver.findElement(By.xpath("//*[@id=\"nav-your-amazon\"]")).isDisplayed();
+		boolean b = driver.findElement(By.xpath("//*[@id=\"nav-your-amazon\"]")).isDisplayed();
 		Assert.assertTrue(b);
 	}
-@Test(priority=3)
-public void loginTest() throws InterruptedException {
-	
-	//*[@id="nav-flyout-ya-signin"]/a/span
-	//*[@id="ap_email"]
-	//*[@id="continue"]
-	//*[@id="ap_password"]
-	//*[@id="signInSubmit"]
-	WebElement ele=driver.findElement(By.xpath("//*[@id=\"nav-link-yourAccount\"]/span[2]/span"));
-	
-	Actions action=new Actions(driver);
-	action.moveToElement(ele).build().perform();
-	driver.findElement(By.xpath("//*[@id=\"nav-flyout-ya-signin\"]/a/span")).click();
-	driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("ramkrish23492@gmail.com");
-	driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
-	driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("");
-	driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
-	Thread.sleep(4000);
-	
-	
-}
-	@Test(priority=4)
-	public void logoutTest() {
-		WebElement ele=driver.findElement(By.xpath("//*[@id=\"nav-link-yourAccount\"]/span[2]/span"));
-		
-		Actions action=new Actions(driver);
+
+	@Test(priority = 3)
+	public void loginTest() throws InterruptedException {
+
+		// *[@id="nav-flyout-ya-signin"]/a/span
+		// *[@id="ap_email"]
+		// *[@id="continue"]
+		// *[@id="ap_password"]
+		// *[@id="signInSubmit"]
+		WebElement ele = driver.findElement(By.xpath("//*[@id=\"nav-link-yourAccount\"]/span[2]/span"));
+
+		Actions action = new Actions(driver);
 		action.moveToElement(ele).build().perform();
-		
-	
-		driver.findElement(By.xpath("//*[@id=\"nav-item-signout-sa\"]/span")).click();
-	
+		driver.findElement(By.xpath("//*[@id=\"nav-flyout-ya-signin\"]/a/span")).click();
+		driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("ramkrish23492@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("");
+		driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
+		Thread.sleep(4000);
+
 	}
-	
+
+	@Test(priority = 4)
+	public void logoutTest() {
+		WebElement ele = driver.findElement(By.xpath("//*[@id=\"nav-link-yourAccount\"]/span[2]/span"));
+
+		Actions action = new Actions(driver);
+		action.moveToElement(ele).build().perform();
+
+		driver.findElement(By.xpath("//*[@id=\"nav-item-signout-sa\"]/span")).click();
+
+	}
+
 	public void tearDown() {
 		driver.close();
 	}
